101d2383fbe3365c6b0ff6afeddd77e2cbc22d6e
Ubuntu <ubuntu@ip-172-31-38-150.ec2.internal>
2024-06-23 17:13:46 +0000

    Adding notebook and quiz of module 2

---
+++ module-2/notebooks/homework/train.py
@@ -0,0 +1,41 @@
+import os
+import pickle
+import click
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+# Adding tracking uri and creating experiment if does not exist
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("homework_module2")
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+
+    # Adding autolog
+    mlflow.sklearn.autolog()
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    # wrapping the training code with a  with mlflow.start_run()
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+    run_train()
