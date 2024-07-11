c1f14dceb7113cd47f303d16bbc79c27a869b44d
Ubuntu <ubuntu@ip-172-31-38-150.ec2.internal>
2024-07-11 08:14:59 +0000

    Adding module 5 homework

---
+++ module-4/homework/mlops/mlops/homework_04/custom/illuminated_paper.py
@@ -0,0 +1,33 @@
+if 'custom' not in globals():
+    from mage_ai.data_preparation.decorators import custom
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+import os
+
+@custom
+def transform_custom(*args, **kwargs):
+    """
+    args: The output from any upstream parent blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your custom logic here
+    current_directory = os.getcwd()
+    print(f"Current Working Directory: {current_directory}")
+    
+    # List the contents of the current directory
+    directory_contents = os.listdir(current_directory)
+    print("Directory Contents:")
+    for item in directory_contents:
+        print(item)
+    
+    return {}
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
---
+++ module-4/homework/mlops/mlops/homework_04/data_loaders/load_model.py
@@ -0,0 +1,32 @@
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+import pickle
+
+@data_loader
+def load_model(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+    # blocks/load_model.py
+    
+
+    with open('model.bin', 'rb') as f_in:
+        dv, model = pickle.load(f_in)
+    return dv, model
+
+    
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
